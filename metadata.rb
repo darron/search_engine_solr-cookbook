8ae0458d0badbb420e55dfb9df1d6a38bc2af1d5
Darron Froese <darron@froese.org>
2013-04-09 08:39:51 -0600

    Adding proper urls to search engine application cookbook.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "darron@froese.org"
 license          "All rights reserved"
 description      "Installs/Configures search_engine_solr"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.2.0"
 
 depends "java"
 depends "jetty"
