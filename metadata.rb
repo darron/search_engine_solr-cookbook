6bc061c59139d49989f4acc18cb7a33551499222
Darron Froese <darron@froese.org>
2013-04-14 14:07:35 -0600

    Licencing Changes.

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             "search_engine_solr"
 maintainer       "Darron Froese"
 maintainer_email "darron@froese.org"
-license          "All rights reserved"
+license          "apachev2"
 description      "Installs/Configures search_engine_solr"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.4.0"
 
 depends "java"
 depends "jetty"
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,10 +2,6 @@
 # Cookbook Name:: search_engine_solr
 # Recipe:: default
 #
-# Copyright (C) 2013 Darron Froese
-# 
-# All rights reserved - Do Not Redistribute
-#
 
 # Java
 include_recipe "java::default"
